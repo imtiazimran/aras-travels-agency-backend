cdbef376567e0cd45d32488328e3916bd18e3e9c
imtiazimran <imtiazimran@outlook.com>
2024-09-09 03:03:59 +0600

    compailation done

--- dist/app/app.js
+++ dist/app/app.js
@@ -10,13 +10,13 @@ const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const routes_1 = __importDefault(require("./routes"));
 const notFound_1 = __importDefault(require("./middlewares/notFound"));
-app.use(body_parser_1.default.json());
-app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://avvisaagency.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
+app.use(body_parser_1.default.json());
+app.use((0, cookie_parser_1.default)());
 app.use("/api/v1", routes_1.default);
 app.get("/", (req, res) => {
     res.send(`
