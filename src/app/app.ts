a002f0614c12a4eeb0141fc0d19ca4cb627bbc10
imtiazimran <imtiazimran@outlook.com>
2024-09-09 02:57:22 +0600

    cors link added

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,7 +11,7 @@ app.use(cookieParser())
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "https://avvisaagency.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
